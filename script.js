af616fb710060713bcedcce7b28443a6e60c9f58
Reem-Samy <23-101150@students.eui.edu.eg>
2025-12-21 19:43:42 +0200

    secondsection

--- script.js
+++ script.js
@@ -4,8 +4,11 @@ hero.addEventListener("mouseenter", () => {
   hero.classList.add("active");
 });
 
+  const section = document.getElementById("activitySection");
 
-
+  section.addEventListener("mouseenter", () => {
+    section.classList.add("active");
+  });
 
 
 
